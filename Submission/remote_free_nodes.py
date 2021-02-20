9da755420346426f69aff2005d4a9997952c2367
System User <rachitskiy.pyu@phystech.edu>
2021-02-21 00:06:50 +0300

    Изменен subprocess.run(), удален shell=True, теперь комманды bash вводятся через запятую

--- Submission/remote_free_nodes.py
+++ Submission/remote_free_nodes.py
@@ -5,8 +5,10 @@ import random
 def get_free_node(occupied_node):
     '''Возвращает название свободной ноды, на которую возможно отправить задачу'''
 
-    squeue = subprocess.run('ssh shipilov.ab@calc.cod.phystech.edu "squeue"',
-               capture_output=True, shell=True, check=True, text=True).stdout
+#    squeue = subprocess.run('ssh shipilov.ab@calc.cod.phystech.edu "squeue"',
+#               capture_output=True, shell=True, check=True, text=True).stdout
+    squeue = subprocess.run(["ssh", "shipilov.ab@calc.cod.phystech.edu", "'squeue'"],
+               capture_output=True, check=True, text=True).stdout
     content=list(map(lambda x: x.split(), squeue.strip().split('\n')))
     possible_nodes=list(map(lambda x: x[8], content))[1:]
 
@@ -22,8 +24,10 @@ def get_free_node(occupied_node):
             time.sleep(30)
 
         node = random.choice(list(set(possible_nodes) - set(occupied_node)))
-        number_of_processes = len(subprocess.run(f'ssh shipilov.ab@calc.cod.phystech.edu "ssh {node} ps aux | grep shipilov.ab"'
-                            , capture_output=True, shell=True, check=True, text=True).stdout.strip().split('\n'))
+#        number_of_processes = len(subprocess.run(f'ssh shipilov.ab@calc.cod.phystech.edu "ssh {node} ps aux | grep shipilov.ab"'
+#                            , capture_output=True, shell=True, check=True, text=True).stdout.strip().split('\n'))
+        number_of_processes = len(subprocess.run(["ssh", "shipilov.ab@calc.cod.phystech.edu", "ssh", f"{node}", "ps", "aux", "|", "grep", "shipilo"]
+                            , capture_output=True, check=True, text=True).stdout.strip().split('\n'))
 
         occupied_node.append(node)
     print(node, end='')
